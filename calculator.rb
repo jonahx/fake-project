9c18324d2ab0bcab554bc7090bcfe2ddfe0f2a11
Jonah <jonahx@gmail.com>
2021-05-14 17:24:21 -0400

    Add legacy code

---
+++ calculator.rb
@@ -0,0 +1,15 @@
+module Calculator
+  module_function
+  def self.add a, b
+    a + b
+  end
+  def self.sub a, b
+    a - b
+  end
+  def self.mult a, b
+    a * b
+  end
+  def self.div a, b
+    a / b
+  end
+end
---
+++ calculator_spec.rb
@@ -0,0 +1,17 @@
+require 'minitest/autorun'
+require './calculator.rb'
+
+describe 'Calculator' do
+  it 'adds' do
+    assert_equal(Calculator.add(1, 1), 2)
+  end
+  it 'subtracts' do
+    assert_equal(Calculator.sub(1, 1), 0)
+  end
+  it 'multiplies' do
+    assert_equal(Calculator.mult(2, 2), 4)
+  end
+  it 'divides' do
+    assert_equal(Calculator.div(4, 2), 2)
+  end
+end
